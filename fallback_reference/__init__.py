70d5721c5fef598d0d3136aaa56f6d9653f1f896
futouyiba <futouyiba@gmail.com>
2026-08-24 04:40:10 +0800

    Export fallback gate planning helpers

--- fallback_reference/__init__.py
+++ fallback_reference/__init__.py
@@ -1,3 +1,17 @@
-from .state import FallbackSafetyState, TrueNoneSettlement, settle_true_none
+from .state import (
+    FallbackGatePlan,
+    FallbackSafetyState,
+    TrueNoneSettlement,
+    fallback_gate_hits,
+    plan_fallback_gate,
+    settle_true_none,
+)
 
-__all__ = ["FallbackSafetyState", "TrueNoneSettlement", "settle_true_none"]
+__all__ = [
+    "FallbackGatePlan",
+    "FallbackSafetyState",
+    "TrueNoneSettlement",
+    "fallback_gate_hits",
+    "plan_fallback_gate",
+    "settle_true_none",
+]

b02747b1ac377ecc1f569abd09f689dc12e3efc6
futouyiba <futouyiba@gmail.com>
2026-08-24 05:02:13 +0800

    Export replay reference surface

---
+++ replay_reference/__init__.py
@@ -0,0 +1,19 @@
+from .runner import (
+    AuthoritativeEntryResult,
+    RandomAddress,
+    ReplayEntry,
+    ReplayLease,
+    ReplayRun,
+    replay_batch,
+    replay_stepwise,
+)
+
+__all__ = [
+    "AuthoritativeEntryResult",
+    "RandomAddress",
+    "ReplayEntry",
+    "ReplayLease",
+    "ReplayRun",
+    "replay_batch",
+    "replay_stepwise",
+]

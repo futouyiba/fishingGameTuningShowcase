42692b85baaa9fee25ac106d3bda52800edffe55
futouyiba <futouyiba@gmail.com>
2026-08-24 04:58:07 +0800

    Export fallback reference surface

---
+++ fallback_reference/__init__.py
@@ -0,0 +1,19 @@
+from .state import (
+    FallbackGatePlan,
+    FallbackSafetyState,
+    TrueNoneSettlement,
+    fallback_gate_hits,
+    plan_fallback_gate,
+    settle_spawn_commit,
+    settle_true_none,
+)
+
+__all__ = [
+    "FallbackGatePlan",
+    "FallbackSafetyState",
+    "TrueNoneSettlement",
+    "fallback_gate_hits",
+    "plan_fallback_gate",
+    "settle_spawn_commit",
+    "settle_true_none",
+]
